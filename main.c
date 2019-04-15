682982164859e247dc658ec02a86c52e18f2f61c
Emmanuel Alfaro Brenes <ealfarobrenes@hotmail.com>
2019-04-15 09:59:57 -0600

    lee el angulo de un json de ejemplo

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <json-c/json.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char buffer[1024];
+
+    FILE *file = fopen("../example.json","r");
+    fread(buffer, sizeof(buffer), 1, file);
+    fclose(file);
+
+    struct json_object *parsed_json = json_tokener_parse(buffer);
+
+    struct json_object *angle;
+
+    json_object_object_get_ex(parsed_json,"final_angle", &angle);
+
+//    printf("Angle is %s \n", json_object_get_string(angle));
+    printf("Angle is %i \n", json_object_get_int(angle));
+
+
     return 0;
 }
