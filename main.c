078c1ce9e1c740834248da2705a07e9ee08e8368
Emmanuel Alfaro Brenes <ealfarobrenes@hotmail.com>
2019-04-18 14:04:06 -0600

    ejemplo alarma

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#define _GNU_SOURCE
+//#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
@@ -9,114 +9,33 @@
 #include <ucontext.h>
 #include <sched.h>
 
-int counting_function(void *row_to_write){
-    int actual_row_to_write = *(int*)row_to_write;
-    fprintf(stderr,"child writing at row %d \n",actual_row_to_write);
-    char *message = "C:";
-    move(actual_row_to_write, 0);
-    printw("%s",message);
-
-//    fprintf(stderr,"child wrote msg\n");
-//    refresh(); sleep(3); return 0;//tmp
-
-    int offset = strlen(message);
+void handle_signal(int singal_number){
+    printf("Alarm!\n");
+    alarm(1);
+}
 
 
-    for (int i = 0; i < 10; ++i) {
-        move(actual_row_to_write, offset);
-        printw("%d", i);
-        refresh();
-        sleep(1);
+void setup_signal() {
+    struct sigaction action;
+    action.sa_handler = handle_signal;
+    sigemptyset (&action.sa_mask);
+    action.sa_flags = 0;
+
+    sigaction(SIGALRM, &action, NULL); // NULL is the old action, not used here
+    alarm(1);
+    while(1) {
+        printf("Sleeping...\n");
+        usleep(0.3 * 1000000);
 
     }
-    return 0;
 }
-
-int  launch_counter(int *row_to_write, void *stack, int stack_size){
-//    int flags = SIGCHLD;
-    int flags = SIGCHLD | CLONE_VM | CLONE_FILES | CLONE_IO; // use this??
-    int thread_id = clone(counting_function, stack+stack_size, flags , row_to_write);
-    fprintf(stderr,"Created thread id:%d writing at row %d \n", thread_id, *row_to_write);
-    return thread_id;
-}
-
 
 
 int main() {
-    setbuf(stdout, NULL);
 
-    ucontext_t context;
-    int x = 10;
+    setup_signal();
 
-
-
-    getcontext(&context);
-    if(x == 10){
-        printf("After saving x=10\n");
-        x = 5;
-        setcontext(&context);
-    }
-    else{
-        printf("After restoring x = 5\n");
-    }
-
-    printf("X is %d\n",x);
 
     return 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    setbuf(stdout, NULL);
-//    scanf("\n");
-//    initscr();
-//    int stack_size = 1024*1024;
-//
-//    int counters = 2, threads_rows[counters], threads_ids[counters];
-//    void *stacks[counters];
-//    for (int i = 0; i < counters; ++i) {
-//        threads_rows[i] = i;
-//        void *stack = malloc(stack_size);
-//        stacks[i] = stack;
-//        threads_ids[i] = launch_counter(&threads_rows[i], stack, stack_size);
-//    }
-//
-//    // wait childs to terminate
-//    while(counters--){
-//        wait(NULL);
-//    }
-//    // free thread's stacks
-//    for (int i = 0; i < counters ; ++i) {
-//        free(stacks[i]);
-//    }
-//
-//    endwin();
-//    return 0;
 }
