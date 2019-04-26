75fc8bb443f78a91ae0bf8aafd4ca46cffcda7eb
Emmanuel Alfaro Brenes <ealfarobrenes@hotmail.com>
2019-04-25 18:55:56 -0600

    primera version de animacion remota

--- client.c
+++ client.c
@@ -7,6 +7,19 @@
 #include "ncurses.h"
 #include "lib/area.h"
 
+
+void display_data(char *data, area terminal_size) {
+    int i = 0; // index to
+    for (int row = 0; row < terminal_size.height; row++) {
+        for (int column = 0; column < terminal_size.width ; column++) {
+            move(row, column);
+            printw("%c", data[i]);
+            i++;
+        }
+    }
+    refresh();
+}
+
 void full_terminal(area terminal_size, char symbol){
     for (int row = 0; row < terminal_size.height; ++row) {
         for (int column = 0; column < terminal_size.width; ++column) {
@@ -37,7 +50,7 @@ int main(){
     refresh();
 
     // read assigned dimension
-    char buffer[100];
+    char buffer[500];
     recv(socket_fd, buffer, sizeof(buffer), NO_FLAGS);
     area assigned_size;
     sscanf(buffer,"%d,%d", &assigned_size.height, &assigned_size.width);
@@ -47,10 +60,16 @@ int main(){
     resizeterm(assigned_size.height, assigned_size.width);
     refresh();
 
-    // wait
-    sleep(2);
-
+    // start receiving data from the server
+    int bytes_received;
+    while(1){
+        bytes_received = recv(socket_fd, buffer, sizeof(buffer), NO_FLAGS);
+        if(bytes_received == 1) break;
+        display_data(buffer, assigned_size);
+    }
     endwin();
+    
+    printf("End of animation\n");
 
 
     return 0;
--- server.c
+++ server.c
@@ -5,6 +5,8 @@
 #include "lib/point.h"
 
 #define MAX_CLIENTS 2
+#define EMPTY_CHAR '-'
+
 
 int start_server(){
     int socket_fd = create_socket();
@@ -41,7 +43,66 @@ void spread_size(area dimension, int *clients_sockets, int num_of_clients){
     }
 }
 
+void clean_canvas(char *canvas, area dimension) {
+    for (int i = 0; i < dimension.height; ++i) {
+        for (int j = 0; j < dimension.width; ++j) {
+            *(canvas+i*dimension.width+j ) = EMPTY_CHAR;
+        }
+    }
+}
+
+void print_char_matrix(char *matrix, area dimension){
+    char c;
+    for (int i = 0; i < dimension.height; ++i) {
+        for (int j = 0; j < dimension.width; ++j) {
+            c = *(matrix+i*dimension.width + j);
+            printf("%c ",c);
+        }
+        printf("\n");
+    }
+}
+
+void update_monitors(int sockets_to_clients[], char *canvas, area dimension){
+    char message[500]; // this will hold all the characters to be sent
+
+    // serialize the canvas to message (copy each row to message)
+    int offset = 0;
+    for (int i = 0; i < dimension.height; i++) {
+        strncpy(message+offset, canvas+offset, dimension.width);
+        offset += dimension.width;
+    }
+
+
+    // send the message to the monitors
+    for (int i = 0; i < sizeof(sockets_to_clients); ++i) {
+        send(sockets_to_clients[i], message, strlen(message), NO_FLAGS);
+    }
+}
+
+
+void test(){
+    // test cleaning canvas
+//    setbuf(stdout, NULL);
+//    area dimension = {4,4};
+//    char canvas[dimension.height][dimension.width];
+//    print_char_matrix(canvas, dimension);
+//    clean_canvas(canvas, dimension);
+//    print_char_matrix(canvas, dimension);
+//    exit(0);
+
+    // test serializing matrix
+    char *canvas = "abcdefghi";
+    area a = {3,3};
+    print_char_matrix(canvas,a);
+
+    update_monitors(NULL, canvas, a);
+    exit(0);
+}
+
 int main() {
+
+//    test();
+    setbuf(stdout, NULL);
 
     int socket_fd = start_server();
     printf("Server running\n");
@@ -55,4 +116,24 @@ int main() {
 
     // send the size to all clients
     spread_size(dimension, clients_sockets, MAX_CLIENTS);
+
+
+    char canvas[dimension.height][dimension.width];
+    clean_canvas(canvas, dimension);
+
+    for (int i = 0; i < dimension.width; ++i) {
+        canvas[0][i] = 'X';
+        printf("Char written\n");
+        update_monitors(clients_sockets, canvas, dimension); //TODO: pass canvas distributions as param?
+        printf("State sent\n");
+        sleep(1);
+        canvas[0][i] = EMPTY_CHAR;
+
+
+    }
+
+    char end = 0;
+    send(clients_sockets[0],&end,1,NO_FLAGS);
+
 }
+
