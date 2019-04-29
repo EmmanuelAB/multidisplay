1f4fb772839bb56a84bb95bddee7e1fa03cc681e
Emmanuel Alfaro Brenes <ealfarobrenes@hotmail.com>
2019-04-29 15:32:44 -0600

    lista enlazada probada

---
+++ list.c
@@ -0,0 +1,109 @@
+//
+// Created by emmanuel on 29/04/19.
+//
+
+#include "list.h"
+
+list *list_create(const char *format){
+    list *a_new_list = malloc(sizeof(list));
+    a_new_list->first = NULL;
+    a_new_list->last = NULL;
+    a_new_list->size = 0;
+    a_new_list->format = format;
+    return a_new_list;
+}
+
+void list_add_element(list *a_list, TYPE element){
+    // create a new node to insert
+    node *new_node = malloc(sizeof(node));
+
+    // set the end of the list
+    new_node->next = NULL;
+
+    // set the value of the new node
+    new_node->value = element;
+
+    if(a_list->first == NULL)
+        a_list->last = a_list->first = new_node;
+
+    else
+        a_list->last->next = new_node;// link the new element
+        a_list->last = new_node;
+
+    // update list size
+    a_list->size++;
+}
+
+
+void list_print(list *a_list){
+    node *current = a_list->first;
+    printf("[");
+    while(current != NULL){
+        // print current element
+        printf(a_list->format, current->value);
+
+        if (current->next != NULL){
+            // there is elements left
+            printf(", ");
+        }
+
+        // pass to the next element
+        current = current->next;
+    }
+    printf("]\n");
+}
+
+// return 0 if found and deleted or -1 if no found
+int list_remove_element_at(list *a_list, int index){
+    int return_value = -1;
+    int current_index = 0;
+    int element_deleted = 0;
+    node *current = a_list->first;
+    node *prior_node = current;
+    while (current != NULL && ! element_deleted) {
+        if(current_index == index){
+            // special_case
+            if(index == 0)
+                a_list->first = current->next;
+            else
+                prior_node->next = current->next;
+            a_list->size--;
+            element_deleted = 1;
+            return_value = 0;
+            free(current);
+
+        }else{
+            prior_node = current;
+            current = current->next;
+            current_index++;
+        }
+    }
+    return return_value;
+}
+
+int list_get_index_of(list *a_list, TYPE value_looking_for) {
+    int return_index = -1;
+    int current_index = 0;
+    node *current = a_list->first;
+    while (current != NULL && return_index == -1) {
+        if(current->value == value_looking_for)
+            return_index = current_index;
+        current = current->next;
+        current_index++;
+    }
+    return return_index;
+}
+
+TYPE list_get_element_at(list *a_list, int index){
+    TYPE return_value;
+    int current_index = 0;
+    int element_found = 0;
+    node *current = a_list->first;
+    while (current != NULL && ! element_found) {
+        if(current_index == index)
+            return_value = current->value;
+        current = current->next;
+        current_index++;
+    }
+    return return_value;
+}
--- main.c
+++ main.c
@@ -1,6 +1,80 @@
 #include <stdio.h>
+#include "list.h"
+
+void test_ints(){
+    list *a_list = list_create("%d");
+
+    for (int i = 0; i < 10; ++i) {
+        list_add_element(a_list, i);
+        printf("test %d = %d\n", i, list_get_index_of(a_list, i));
+    }
+
+    list_print(a_list);
+    list_remove_element_at(a_list, 0);
+    list_print(a_list);
+    list_remove_element_at(a_list, 1);
+    list_print(a_list);
+    list_remove_element_at(a_list, 2);
+    list_print(a_list);
+    list_remove_element_at(a_list, 3);
+    list_print(a_list);
+    list_remove_element_at(a_list, 5);
+    list_print(a_list);
+
+    printf("Deleting invalid index %d\n", list_remove_element_at(a_list, 50));
+
+    printf("Deleting all elements\n");
+    while(a_list->size){
+        printf("Removing %d \n", list_get_element_at(a_list, 0));
+        list_remove_element_at(a_list, 0);
+        list_print(a_list);
+
+    }
+    printf("All removed\n");
+    list_print(a_list);
+    printf("Adding one more\n");
+    list_add_element(a_list, 10);
+    list_print(a_list);
+    exit(0);
+}
+
+void test_floats(){
+    list *a_list = list_create("%.1f");
+
+    for (float i = 0; i < 10; i++) {
+        list_add_element(a_list, i);
+        printf("test %.1f = %d\n", i, list_get_index_of(a_list, i));
+    }
+
+    list_print(a_list);
+    list_remove_element_at(a_list, 0);
+    list_print(a_list);
+    list_remove_element_at(a_list, 1);
+    list_print(a_list);
+    list_remove_element_at(a_list, 2);
+    list_print(a_list);
+    list_remove_element_at(a_list, 3);
+    list_print(a_list);
+    list_remove_element_at(a_list, 5);
+    list_print(a_list);
+
+    printf("Deleting invalid index %d\n", list_remove_element_at(a_list, 50));
+
+
+
+    list_print(a_list);
+    exit(0);
+}
+
 
 int main() {
-    printf("Hello, World!\n");
+    test_ints();
+
+//    test_floats();
+
+
+
+
+
     return 0;
 }
